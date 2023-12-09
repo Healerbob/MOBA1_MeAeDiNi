30e1ec21169b1379f69041b743d9659f40f12c3c
Healerbob <p.mueller@muero.ch>
2023-12-09 19:29:03 +0100

    Updated Board rendering

--- Sorry/app/src/main/java/com/example/myapplication/GameBoard.kt
+++ Sorry/app/src/main/java/com/example/myapplication/GameBoard.kt
@@ -1,22 +1,21 @@
 package com.example.myapplication
 
 class GameBoard {
-    val menschAergereDichNichtSpielfeld = arrayOf(
-        arrayOf("r", "r", "0", "0", "F", "F", "S", "0", "0", "b", "b"),
-        arrayOf("r", "r", "0", "0", "F", "Z", "F", "0", "0", "b", "b"),
-        arrayOf("0", "0", "0", "0", "F", "Z", "F", "0", "0", "0", "0"),
-        arrayOf("0", "0", "0", "0", "F", "Z", "F", "0", "0", "0", "0"),
-        arrayOf("S", "F", "F", "F", "F", "Z", "F", "F", "F", "F", "F"),
-        arrayOf("F", "Z", "Z", "Z", "Z", "0", "Z", "Z", "Z", "Z", "F"),
-        arrayOf("F", "F", "F", "F", "F", "Z", "F", "F", "F", "F", "S"),
-        arrayOf("0", "0", "0", "0", "F", "Z", "F", "0", "0", "0", "0"),
-        arrayOf("0", "0", "0", "0", "F", "Z", "F", "0", "0", "0", "0"),
-        arrayOf("y", "y", "0", "0", "F", "Z", "F", "0", "0", "g", "g"),
-        arrayOf("y", "y", "0", "0", "S", "F", "F", "0", "0", "g", "g"),
-        arrayOf("0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"),
-        arrayOf("0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0")
+    private val menschAergereDichNichtSpielfeld = arrayOf(
+        arrayOf("r0", "r1", "0", "0", "F", "F", "bs", "0", "0", "b0", "b1"),
+        arrayOf("r2", "r3", "0", "0", "F", "ba", "F", "0", "0", "b2", "b3"),
+        arrayOf("0", "0", "0", "0", "F", "bb", "F", "0", "0", "0", "0"),
+        arrayOf("0", "0", "0", "0", "F", "bc", "F", "0", "0", "0", "0"),
+        arrayOf("rs", "F", "F", "F", "F", "bd", "F", "F", "F", "F", "F"),
+        arrayOf("F", "ra", "rb", "rc", "rd", "0", "gd", "gc", "gb", "ga", "F"),
+        arrayOf("F", "F", "F", "F", "F", "yd", "F", "F", "F", "F", "gs"),
+        arrayOf("0", "0", "0", "0", "F", "yc", "F", "0", "0", "0", "0"),
+        arrayOf("0", "0", "0", "0", "F", "yb", "F", "0", "0", "0", "0"),
+        arrayOf("y0", "y1", "0", "0", "F", "ya", "F", "0", "0", "g0", "g1"),
+        arrayOf("y2", "y3", "0", "0", "ys", "F", "F", "0", "0", "g2", "g3"),
     )
-    fun getSpielfeld() : Array<Array<String>> {
+
+    fun getSpielfeld(): Array<Array<String>> {
         return menschAergereDichNichtSpielfeld
     }
 }
--- Sorry/app/src/main/java/com/example/myapplication/GameBoardRenderer.kt
+++ Sorry/app/src/main/java/com/example/myapplication/GameBoardRenderer.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,12 +21,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 class GameBoardRenderer {
 
     @Composable
+    @Preview
     fun getSpielfeldFromGameBoard() {
         val gameBoard = GameBoard()
         val spielfeld = gameBoard.getSpielfeld()
@@ -33,21 +42,25 @@ class GameBoardRenderer {
 
     @Composable
     fun GridDemo(gridArray: Array<Array<String>>) {
-        LazyColumn(
+        Box (
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(4.dp) // Reduziere das Padding
+                .background(Color.Gray)
         ) {
-            items(gridArray.size) { rowIndex ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 1.dp),
-                    horizontalArrangement = Arrangement.spacedBy(1.dp)
-                ) {
-                    gridArray[rowIndex].forEach { value ->
-                        GridCell(value = value) { clickedValue ->
-                            Log.d("CellClicked", "Cell clicked: $clickedValue")                    }
+            LazyColumn (
+                modifier = Modifier
+                    .border(2.dp, Color.Black)
+                    .background(Color(255, 255, 180))
+                    .padding(20.dp)
+            ) {
+                items(gridArray.size) { rowIndex ->
+                    Row {
+                        gridArray[rowIndex].forEach { value ->
+                            GridCell(value) { clickedValue ->
+                                Log.d("CellClicked", "Cell clicked: $clickedValue")
+                            }
+                        }
                     }
                 }
             }
@@ -58,55 +71,51 @@ class GameBoardRenderer {
     @Composable
     fun GridCell(value: String, onCellClicked: (String) -> Unit) {
         Box(
-            modifier = Modifier
-                .size(30.dp)
-                .padding(1.dp)
-                .background(getCellColor(value))
-                .clip(MaterialTheme.shapes.medium)
-                .clickable {
-                    onCellClicked(value)
-                }
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(32.dp)
         ) {
-            val textColor = when (value) {
-                "S", "Z", "F" -> Color.Black
-                else -> Color.White
+            if ("0" != value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(0.9f)
+                        .border(1.dp, Color.Black, CircleShape)
+                        .background(getCellColor(value), CircleShape)
+                        .clickable {
+                            onCellClicked(value)
+                        }
+                ) {
+                    Text(
+                        text = getCellSymbol(value),
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
             }
-
-            Text(
-                text = getCellSymbol(value),
-                color = textColor,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.Center)
-            )
         }
     }
 
 
-
     @Composable
     fun getCellColor(value: String): Color {
-        return when (value) {
-            "S" -> Color(255, 223, 0) // Gold
-            "Z" -> Color.Gray
-            "F" -> Color.Cyan
-            "g" -> Color.Green
-            "b" -> Color.Blue
-            "y" -> Color.Yellow
-            "r" -> Color.Red
+        return when {
+            "F" == value -> Color.White
+            "g.".toRegex().matches(value) -> Color.Green
+            "b.".toRegex().matches(value) -> Color.Blue
+            "y.".toRegex().matches(value) -> Color.Yellow
+            "r.".toRegex().matches(value) -> Color.Red
             else -> MaterialTheme.colorScheme.background
         }
     }
 
     @Composable
     fun getCellSymbol(value: String): String {
-        return when (value) {
-            "S" -> "S"
-            "Z" -> "Z"
-            "F" -> ""
-            "g" -> "g"
-            "b" -> "b"
-            "y" -> "y"
-            "r" -> "r"
+        return when {
+            ".s".toRegex().matches(value) -> "A"
+            ".a".toRegex().matches(value) -> "a"
+            ".b".toRegex().matches(value) -> "b"
+            ".c".toRegex().matches(value) -> "c"
+            ".d".toRegex().matches(value) -> "d"
             else -> ""
         }
     }
